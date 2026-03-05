2e1c5345f9529bebb0925c258ff24be7d5e979cc
mathRyan889 <matheusryan889@gmail.com>
2026-03-05 16:26:22 -0300

    primeiro commit

---
+++ cars/views.py
@@ -0,0 +1,19 @@
+from dj_rql.drf import RQLFilterBackend
+from rest_framework import viewsets
+from cars.filters import BrandFilterClass, CarFilterClass
+from cars.models import Brand, Car
+from cars.serializers import BrandModelSerializer, CarModelSerializer
+
+
+class BrandModelViewSet(viewsets.ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandModelSerializer
+    filter_backends = [RQLFilterBackend]
+    rql_filter_class = BrandFilterClass
+
+
+class CarModelViewSet(viewsets.ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarModelSerializer
+    filter_backends = [RQLFilterBackend]
+    rql_filter_class = CarFilterClass
